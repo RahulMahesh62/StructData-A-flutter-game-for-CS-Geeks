16b66ecb701cd13a28f3ddf2b6d6130f9bd7e2da
Rahul Mahesh <41413029+RahulMahesh62@users.noreply.github.com>
2020-04-01 22:25:18 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,34 @@ class _QuizPageState extends State<QuizPage> {
 
   List<Icon> scoree = [];
 
+  List<String> questions = [
+    'A stack is called a LIFO data structure.',
+    'An array is a random access data structure; a stack is not.',
+    'Stacks can be used to print a list backwards without using recursion.',
+    'You cannot write a recursive algorithm to implement a sequential search algorithm.',
+    'To access the fifth element in a list, we must first traverse the first four elements.',
+    'The recursive algorithm must have three or more base cases.',
+    'A function that calls itself is an iterative function.',
+    'A queue cannot be implemented using in an array.',
+    'Every node in a linked list has two components: one to store the relevant information and one to store the address.T',
+    'Building a linked list backwards places the new item to be added at the end of the linked list.',
+  ];
+
+  List<bool> answers = [
+    true,
+    true,
+    true,
+    false,
+    true,
+    false,
+    false,
+    false,
+    true,
+    false,
+  ];
+
+  int questionn=0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -41,7 +69,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionn],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -65,13 +93,15 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAns = answers[questionn];
+                if(correctAns==true){
+                  print('right');
+                }
+                else{
+                  print('false');
+                }
                 setState(() {
-                  scoree.add(
-                    Icon(
-                      Icons.check,
-                    color: Colors.green,
-                  ),
-                  );
+                  questionn++;
                 });
               },
             ),
@@ -90,13 +120,15 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAns = answers[questionn];
+                if(correctAns==false){
+                  print('right');
+                }
+                else{
+                  print('false');
+                }
                 setState(() {
-                  scoree.add(
-                    Icon(
-                      Icons.close,
-                    color: Colors.red,
-                  ),
-                  );
+                  questionn++;
                 });
               },
             ),
